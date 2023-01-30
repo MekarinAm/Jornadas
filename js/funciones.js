79d2d817d7eab769b478f08ca0ba1405db18836a
MekarinAm <120043841+MekarinAm@users.noreply.github.com>
2023-01-30 09:50:15 -0600

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,76 @@
+function c1()
+{
+    document.getElementById("switch1").checked=true;
+    document.getElementById("switch2").checked=false;
+    document.getElementById("switch3").checked=false;
+    document.getElementById("switch4").checked=false;
+    document.getElementById("switch5").checked=false;
+    document.getElementById("switch6").checked=false;
+    document.getElementById("switch7").checked=false;
+}
+
+function c2()
+{
+    document.getElementById("switch1").checked=false;
+    document.getElementById("switch2").checked=true;
+    document.getElementById("switch3").checked=false;
+    document.getElementById("switch4").checked=false;
+    document.getElementById("switch5").checked=false;
+    document.getElementById("switch6").checked=false;
+    document.getElementById("switch7").checked=false;
+}
+
+function c3()
+{
+    document.getElementById("switch1").checked=false;
+    document.getElementById("switch2").checked=false;
+    document.getElementById("switch3").checked=true;
+    document.getElementById("switch4").checked=false;
+    document.getElementById("switch5").checked=false;
+    document.getElementById("switch6").checked=false;
+    document.getElementById("switch7").checked=false;
+}
+
+function c4()
+{
+    document.getElementById("switch1").checked=false;
+    document.getElementById("switch2").checked=false;
+    document.getElementById("switch3").checked=false;
+    document.getElementById("switch4").checked=true;
+    document.getElementById("switch5").checked=false;
+    document.getElementById("switch6").checked=false;
+    document.getElementById("switch7").checked=false;
+}
+
+function c5()
+{
+    document.getElementById("switch1").checked=false;
+    document.getElementById("switch2").checked=false;
+    document.getElementById("switch3").checked=false;
+    document.getElementById("switch4").checked=false;
+    document.getElementById("switch5").checked=true;
+    document.getElementById("switch6").checked=false;
+    document.getElementById("switch7").checked=false;
+}
+
+function c6()
+{
+    document.getElementById("switch1").checked=false;
+    document.getElementById("switch2").checked=false;
+    document.getElementById("switch3").checked=false;
+    document.getElementById("switch4").checked=false;
+    document.getElementById("switch5").checked=false;
+    document.getElementById("switch6").checked=true;
+    document.getElementById("switch7").checked=false;
+}
+
+function c7()
+{
+    document.getElementById("switch1").checked=false;
+    document.getElementById("switch2").checked=false;
+    document.getElementById("switch3").checked=false;
+    document.getElementById("switch4").checked=false;
+    document.getElementById("switch5").checked=false;
+    document.getElementById("switch6").checked=false;
+    document.getElementById("switch7").checked=true;
+}
